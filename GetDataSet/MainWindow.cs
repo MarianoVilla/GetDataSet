96640aea45c4a1666f068f57194646fc0a6917f7
Mariano Villa <Mariano.Villa@outlook.com.ar>
2020-01-24 10:43:16 -0300

    Add project files.

---
+++ GetDataSet/MainWindow.cs
@@ -0,0 +1,34 @@
+﻿namespace GetDataSet
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("98d8bad6-de18-4333-99ec-e5f2886e8cb6")]
+    public class MainWindow : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow() : base(null)
+        {
+            this.Caption = "MainWindow";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new MainWindowControl();
+        }
+    }
+}
